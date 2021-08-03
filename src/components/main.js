675095988869ebf248d9f1ae9633616f89a3737c
Dhiraj Bag <dhirajbag.db@gmail.com>
2021-08-03 20:46:08 +0530

    href fix

--- src/components/main.js
+++ src/components/main.js
@@ -3,8 +3,6 @@ import {React, Component} from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
-import { encrypt } from '../algorithms/encryption';
-import { decrypt } from '../algorithms/decryption';
 import Encryption from './encrypt-interface';
 import Decryption from './decrypt-interface';
 class Main extends Component{
@@ -16,7 +14,7 @@ class Main extends Component{
                 <Switch>
                     <Route path="/encrypt" component={Encryption}/>
                     <Route path="/decrypt" component={Decryption}/>
-                    <Redirect to="/encrypt"/>
+                    <Redirect to="./encrypt"/>
                 </Switch>
                 <Footer/>
                 
